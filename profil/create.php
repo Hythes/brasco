ffcf02be9d3863dd01768c609ae91eb7c629a279
hythes <hanifsetyananda17@gmail.com>
2019-07-29 22:15:09 +0700

    kedua

---
+++ profil/create.php
@@ -0,0 +1,13 @@
+<input type="number" name="kode_cabang" max="3">
+<input type="text" name="nama_cabang" max="40">
+<input type="text" name="alamat_1" max="40">
+<input type="text" name="alamat_2" max="40">
+<input type="text" name="kota_cabang" max="30">
+<input type="number" name="kode_pos">
+<input type="number" name="no_telepon_cabang" max="12">
+<input type="number" name="no_telepon" max="14">
+<input type="text" name="chief_store_manager" max="20">
+<input type="file" name="foto">
+<button name="save"></button>
+<button name="cancel"></button>
+<button name="close"></button>
