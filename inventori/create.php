91c4bf6c046e0887365697365e4ea83a0aec85f7
hythes <hanifsetyananda17@gmail.com>
2019-07-29 22:12:22 +0700

    coba

---
+++ inventori/create.php
@@ -0,0 +1,10 @@
+<input type="text" name="barcode" required>
+<button name="cek_duplikasi"></button>
+<input type="text" name="nama_barang" required> 
+<input type="text" name="satuan" required>
+<select name="tipe_barang"></select>
+<input type="text" name="harga_1">
+<input type="text" name="harga_2">
+<input type="text" name="harga_3">
+<button name="add"></button>
+<button name="cancel"></button>
---
+++ inventori/index.php
@@ -0,0 +1,7 @@
+<button name="copy"></button>
+<button name="csv"></button>
+<button name="excel"></button>
+<button name="pdf"></button>
+<button name="print"></button>
+
+<!-- Terserah front end untuk ngisi table -->
